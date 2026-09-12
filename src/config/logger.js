4912cf7b6ab832bab037bb457c500d31b03fca8f
riancintiyo <cintiyo.ryan@gmail.com>
2026-09-12 20:26:50 +0700

    Feat: Loggersetup

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,7 +2,7 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.combined((
+  format: winston.format.combine((
     winston.format.timestamp(),
     winston.format.errors({ stack: true }),
     winston.format.json()
